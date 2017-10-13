925b66327362b82db3549d8aff886dd1591a65a0
Vitalii Kulyk <vitcool@ukr.net>
2017-10-13 09:04:26 +0300

    elkn-4: extendable list added for main page

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import ReactExpandableListView from "react-expandable-listview";
+import { DATA } from "../Mocks/const";
 
 export default class Main extends React.Component {
-    render(){
-        return(
-            <div>Hello, i am app js</div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <div>Hello, i am app js</div>
+        <ReactExpandableListView
+          data={DATA}
+          headerAttName="headerName"
+          itemsAttName="items"
+        />
+      </div>
+    );
+  }
 }
